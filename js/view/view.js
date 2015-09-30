6a65e40d1224a5ce85671cfe8934d5d9236f14c1
rajalakshmi <rajalakshmi0589@gmail.com>
2015-09-30 11:21:06 +0530

    login submit

--- js/view/view.js
+++ js/view/view.js
@@ -1,23 +1,25 @@
 window.LoginView = Backbone.View.extend({
-
 	initialize : function() {
+		var self = this;
 		console.log('Initializing Login View');
 	},
-
 	events: {
 		"click #login_button" : "login_submit"
 	},
 	login_submit: function(){
-		console.log("login submitted");
-		alert("submitted");
+
+		user = this.$('#username').val();
+		pwd = this.$('#password').val();
+		alert("You logged in as " + user + " and a password of " + pwd);
+		return false;
 	},
 	render : function() {
 		imagesdiv = "";
 		data = this.collection;
 		currTemplate = this.template;
 		data.each(function(iModel, index, list) {
-			imagesdiv = imagesdiv + currTemplate(iModel.toJSON());
+		imagesdiv = imagesdiv + currTemplate(iModel.toJSON());
 		});
 		$(this.el).html(imagesdiv);
-	},
+}
 });
